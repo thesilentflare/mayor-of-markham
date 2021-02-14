be2075d6cddb0ae8a073206eaf1c6983d54d7650
Gavin L <gawainl26@gmail.com>
2021-02-13 19:15:16 -0500

    more gameloop code

--- server/Game/mayor.js
+++ server/Game/mayor.js
@@ -1,8 +1,8 @@
-const deck = [];
-const discardStack1 = [];
-const discardStack2 = [];
-const playerQueue = [];
-const sheriff = "";
+let deck = [];
+let discardStack1 = [];
+let discardStack2 = [];
+let playerQueue = [];
+let sheriffIndex = 0;
 
 const startGame = ({ rounds, players, socket, player }, io) => {
 	if (players.length < 2) return { error: "Not enough players" };
@@ -14,36 +14,67 @@ const startGame = ({ rounds, players, socket, player }, io) => {
 	//socket.broadcast.to(player.room).emit("gameStart");
 	//console.log(rounds);
 
+	// Set starting currencies
+	// populate player points
+	playerQueue = players.map((players) => ({
+		...players,
+		points: 0,
+	}));
+	console.log(playerQueue);
+
+	// populate card deck???
+
 	// Main game LOOOP
 	for (i = 1; i <= rounds; i++) {
+		// Msg game round
 		io.to(player.room).emit("message", {
 			player: "admin",
 			text: `Round ${i}`,
 		});
-        
-        // Set starting currencies
-        // Set sheriff
-        // Set turn order order
 
-        // Loop tru merchant players
-            // Discard Action
-            // Draw Action
-            // Place into bag Action
-            // Start Check Phase
-                // Check/not check Actions
-            // Calculate
-            // Set next Sheriff
+		// If first round everyone draws 6 cards default
 
+		//Msg you are sheriff to sheriff at round start
+		io.to(playerQueue[sheriffIndex].id).emit("message", {
+			player: "admin",
+			text: `You Are the Sheriff this Round`,
+		});
 
+		// console.log(players);
+		// somehow figure out how to msg wait to those that is not their turn
+
+		// Loop tru merchant players
+		for (j = 0; j < playerQueue.length; j++) {
+			// Only merchant players
+			if (j !== sheriffIndex) {
+				// Discard Action
+				io.to(playerQueue[j].id).emit("message", {
+					player: "admin",
+					text: "Discard",
+				});
+				// Draw Action
+				// Place into bag Action
+			}
+		}
+
+		// Start Check Phase
+		// Check/not check Actions
+		// Calculate
+
+		// Set next Sheriff
+		if (sheriffIndex === playerQueue.length - 1) {
+			sheriffIndex = 0;
+		} else {
+			sheriffIndex++;
+		}
 	}
 
-    // End of game
-    // Calculate final counts
-    // Send winner info
+	// End of game
+	// Calculate final counts
+	// Send winner info
 
 	return { player };
 };
-
 
 module.exports = {
 	startGame,
